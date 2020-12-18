35b93d889e2ca3322e30ff3f1de9add9b662eee7
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2020-12-18 11:42:24 -0600

    updated pages

---
+++ src/components/AboutPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+    render() { 
+        return (
+            <div>
+            <h1>About Us</h1>
+                <p>This is the final bastion</p>
+                <p>The last known refuge</p>
+                <p>The one remaining edifice to salvation</p> <p>and the code</p>
+                <p>still</p>
+                <p>don't work</p>
+                <p>Pain.</p>
+            </div>
+        )
+    }
+ }
+
+
+
+export default AboutPage;
---
+++ src/components/MenuPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class MenuPage extends Component { 
+    render() { 
+        return (
+            <div>
+                <h1>Menu</h1>
+                <h2> Check out our amazing 24/7 menu:</h2>
+                <h3>!We got fresh hot react router!</h3>
+                <p>Rockin Reactin Router Ranch Wraps</p>
+                <p>Rip Rarin' Router Rodeo Ribs w/ React Remoulade</p>
+                <p>Router Salad</p>
+                <p>React Soup</p>
+
+            </div>
+        
+        )
+    }
+}
+
+export default MenuPage;
